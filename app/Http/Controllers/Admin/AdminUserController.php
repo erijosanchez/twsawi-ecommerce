a11012d209131bfea10763a5df29100e2132f645
Erick José Sanchez Pinedo <erijito.2@gmail.com>
2025-08-16 17:26:53 -0500

    Actualización de vista de perfil de admin

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -95,6 +95,7 @@ class AdminUserController extends Controller
             //'email' => 'required|email|unique:users,email,' . $user->id,
             'phone' => 'nullable|string|max:15',
             'birth_date' => 'nullable|date|before:today',
+            'gender' => 'nullable|string|'
         ], [
             /** 'email.required' => 'El correo electrónico es obligatorio.',
             *'email.email' => 'El correo electrónico no es válido.',
@@ -110,6 +111,7 @@ class AdminUserController extends Controller
                 //'email' => $request->email,
                 'phone' => $request->phone,
                 'birth_date' => $request->birth_date,
+                'gender' => $request->gender,
                 
             ]);
             return redirect()->route('admin.profile')->with('success', 'Datos del perfil actualizados correctamente.');
@@ -131,6 +133,41 @@ class AdminUserController extends Controller
     {
         $user = User::findOrFail($id);
         return view('admin.pages.users.edit', compact('user'));
+    }
+
+    public function updateUser($id, Request $request)
+    {
+        $user = User::findOrFail($id);
+        
+        $request->validate([
+            //'email' => 'required|email|unique:users,email,' . $user->id,
+            'phone' => 'nullable|string|max:15',
+            'birth_date' => 'nullable|date|before:today',
+            'gender' => 'nullable|string|'
+        ], [
+            /** 'email.required' => 'El correo electrónico es obligatorio.',
+            *'email.email' => 'El correo electrónico no es válido.',
+            *'email.unique' => 'El correo electrónico ya está en uso.',*/
+            'birth_date.date' => 'La fecha de nacimiento no es válida.',
+            'birth_date.before' => 'La fecha de nacimiento debe ser una fecha pasada.',
+            'phone.max' => 'El número de teléfono no debe exceder los 15 caracteres.',
+            //'phone.regex' => 'El número de teléfono contiene caracteres no válidos.',
+        ]);
+
+        try {
+            $user->update([
+                //'email' => $request->email,
+                'phone' => $request->phone,
+                'birth_date' => $request->birth_date,
+                'gender' => $request->gender,
+                
+            ]);
+            return redirect()->route('admin.profile')->with('success', 'Datos del perfil actualizados correctamente.');
+        } catch (\Exception $e) {
+            return redirect()->back()
+                ->with('error', 'Error al actualizar los datos del perfil: ' . $e->getMessage())
+                ->withInput();
+        }
     }
     
     public function destroyUser($id)
