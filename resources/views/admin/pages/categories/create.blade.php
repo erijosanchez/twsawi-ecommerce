00ab16f767e4911dff8dfb6563275948a854dad8
Erick José Sanchez Pinedo <erijito.2@gmail.com>
2025-09-09 12:58:51 -0500

    corrección de la vista de creación de categorías

--- resources/views/admin/pages/categories/create.blade.php
+++ resources/views/admin/pages/categories/create.blade.php
@@ -15,8 +15,7 @@
                                                 <div class="card-rounded card-body">
                                                     <div class="card-body">
                                                         <h4 class="card-title">Crear Nueva Categoría</h4>
-                                                        <form class="form-sample"
-                                                            action="" method="POST">
+                                                        <form class="form-sample" action="" method="POST">
                                                             @csrf
                                                             <p class="card-description">
                                                                 Informacion de la categoría
@@ -24,19 +23,56 @@
                                                             <div class="row">
                                                                 <div class="col-md-6">
                                                                     <div class="form-group p row">
-                                                                        <label class="col-sm-4 col-form-label">Nombre</label>
+                                                                        <label
+                                                                            class="col-sm-4 col-form-label">Nombre</label>
                                                                         <div class="colum-pass col-sm-8">
                                                                             <input type="text" class="form-control"
-                                                                                value=""
-                                                                                name="name" id="name" required />
+                                                                                value="" name="name" id="name"
+                                                                                required />
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-6">
                                                                     <div class="form-group p row">
-                                                                        <label class="col-sm-4 col-form-label">Descripción</label>
+                                                                        <label
+                                                                            class="col-sm-4 col-form-label">Descripción</label>
                                                                         <div class="colum-pass col-sm-8">
-                                                                            <textarea class="form-control" name="" id="" cols="30" rows="10" placeholder="Escribe la descripción..."></textarea>
+                                                                            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                                                                rows="3">{{ old('description') }}</textarea>
+                                                                            @error('description')
+                                                                                <div class="invalid-feedback">
+                                                                                    {{ $message }}</div>
+                                                                            @enderror
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <p class="card-description">
+                                                                SEO
+                                                            </p>
+                                                            <div class="row">
+                                                                <div class="col-md-6">
+                                                                    <div class="form-group p row">
+                                                                        <label class="col-sm-4 col-form-label">Meta
+                                                                            Titulo</label>
+                                                                        <div class="col-sm-8 colum-pass">
+                                                                            <input type="text"
+                                                                                class="form-control @error('meta_title') is-invalid @enderror"
+                                                                                id="meta_title" name="meta_title"
+                                                                                value="{{ old('meta_title') }}">
+                                                                            @error('meta_title')
+                                                                                <div class="invalid-feedback">
+                                                                                    {{ $message }}</div>
+                                                                            @enderror
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-md-6">
+                                                                    <div class="form-group p row">
+                                                                        <label class="col-sm-4 col-form-label">Meta
+                                                                            Descripción</label>
+                                                                        <div class="colum-pass col-sm-8">
+                                                                            
                                                                         </div>
                                                                     </div>
                                                                 </div>
@@ -44,36 +80,44 @@
                                                             <div class="row">
                                                                 <div class="col-md-6">
                                                                     <div class="form-group p row">
-                                                                        <label class="col-sm-4 col-form-label">Fecha de
-                                                                            Nacimiento</label>
-                                                                        <div class="col-sm-8 colum-pass">
-                                                                            <input type="date" name="birth_date"
-                                                                                id="birth_date"
-                                                                                value=""
-                                                                                class="form-control @error('birth_date') is-invalid @enderror">
+                                                                        <label
+                                                                            class="col-sm-4 col-form-label">Imagen</label>
+                                                                        <div class="colum-pass col-sm-8">
+                                                                            <input type="text" class="form-control"
+                                                                                value="" name="name" id="name"
+                                                                                required />
                                                                         </div>
-                                                                        @error('birth_date')
-                                                                            <div class="invalid-feedback">
-                                                                                <i class="fas fa-exclamation-circle"></i>
-                                                                                {{ $message }}
-                                                                            </div>
-                                                                        @enderror
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-6">
                                                                     <div class="form-group p row">
-                                                                        <label class="col-sm-4 col-form-label">Genero
-                                                                        </label>
-                                                                        <div class="col-sm-8 colum-pass">
-                                                                            <select name="gender" id="gender"
-                                                                                class="form-control">
-                                                                                <option value="">Seleccionar género
-                                                                                </option>
-                                                                                
-                                                                                    <option value="">
-                                                                                        f
-                                                                                    </option>
-                                                                            </select>
+                                                                        <label class="col-sm-4 col-form-label">Categoría
+                                                                            Padre</label>
+                                                                        <div class="colum-pass col-sm-8">
+                                                                            <textarea class="form-control" name="" id="" cols="30" rows="10"
+                                                                                placeholder="Escribe la descripción..."></textarea>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-md-6">
+                                                                    <div class="form-group p row">
+                                                                        <label class="col-sm-4 col-form-label">Orden</label>
+                                                                        <div class="colum-pass col-sm-8">
+                                                                            <input type="text" class="form-control"
+                                                                                value="" name="name" id="name"
+                                                                                required />
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-md-6">
+                                                                    <div class="form-group p row">
+                                                                        <label
+                                                                            class="col-sm-4 col-form-label">Estado</label>
+                                                                        <div class="colum-pass col-sm-8">
+                                                                            <textarea class="form-control" name="" id="" cols="30" rows="10"
+                                                                                placeholder="Escribe la descripción..."></textarea>
                                                                         </div>
                                                                     </div>
                                                                 </div>
